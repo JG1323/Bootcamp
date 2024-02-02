ca7663f9806a85497256731eae25615529e0e45f
Joel García Pérez <158473724+JG1323@users.noreply.github.com>
2024-02-02 12:33:21 +0100

    TA2Eclipse

---
+++ Ta2/src/ComentarioLinea.java
@@ -0,0 +1,10 @@
+
+public class ComentarioLinea {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Àdios");
+		System.out.println("FIN DE PROGRAMA");
+	}
+
+}
