0c3f11542573f02255e67446dd1ee7f36c29c8e9
Joel García Pérez <158473724+JG1323@users.noreply.github.com>
2024-02-05 10:07:52 +0100

    Operadores unarios

---
+++ Operador/src/PruebaApp.java
@@ -0,0 +1,12 @@
+
+public class PruebaApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int op=10;
+		System.out.println(op);
+		System.out.println(op++);
+		System.out.println(++op);
+	}
+
+}
