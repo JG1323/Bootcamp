dc436c59ba98fa7ae79708ae80e40d8d1bdfef10
Joel García Pérez <158473724+JG1323@users.noreply.github.com>
2024-02-06 12:39:42 +0100

    bucle for

---
+++ T5/src/TA5bucle2.java
@@ -0,0 +1,13 @@
+
+public class TA5bucle2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+			
+		for (int num=1; num<100; num++) {
+			System.out.println(num);
+		}
+	}
+
+}
