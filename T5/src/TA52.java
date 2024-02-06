22a1574e8da8f6ec4788eacd995c2d090b59a522
Joel García Pérez <158473724+JG1323@users.noreply.github.com>
2024-02-06 12:12:13 +0100

    cambios

--- T5/src/TA52.java
+++ T5/src/TA52.java
@@ -11,7 +11,7 @@ public class TA52 {
 		sc.close();
 
 		
-		System.out.println("Bienvenido" + name.toUpperCase());
+		System.out.println("Bienvenido " + name.toUpperCase());
 			
 	}
 
---
+++ T5/src/TA5JOption.java
@@ -0,0 +1,12 @@
+import javax.swing.JOptionPane;
+public class TA5JOption {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+			String nombre=JOptionPane.showInputDialog("¿ID de usuario?");
+			JOptionPane.showMessageDialog(null, "ID registrada:"+nombre);
+			
+	}
+
+}
