faa6cd02c3c865af8b6be5984a06301ee1e95026
Joel García Pérez <158473724+JG1323@users.noreply.github.com>
2024-02-06 13:54:46 +0100

    cambios errores

--- Ejercicio 2 TA4/src/TA43.java
+++ Ejercicio 2 TA4/src/TA43.java
@@ -6,10 +6,12 @@ public class TA43 {
 		int N=1;
 		System.out.println("El valor de la variable N es:" + N);
 		int suma=N+77;
+		//N=N+77; //78
 		System.out.println(suma);
 		int resta=N-3;
 		System.out.println(resta);
 		int duplicar=N*2;
+		
 		System.out.println(duplicar);
 	}
 
