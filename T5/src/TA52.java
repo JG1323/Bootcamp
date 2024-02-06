7924c634f5995f44c091e36a25bce884165720cf
Joel García Pérez <158473724+JG1323@users.noreply.github.com>
2024-02-06 11:27:32 +0100

    ejercicios

---
+++ T5/src/TA52.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class TA52 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner sc= new Scanner(System.in);
+		System.out.println("¿Nombre?");
+		String name= sc.nextLine();
+		sc.close();
+
+		
+		System.out.println("Bienvenido" + name.toUpperCase());
+			
+	}
+
+}
