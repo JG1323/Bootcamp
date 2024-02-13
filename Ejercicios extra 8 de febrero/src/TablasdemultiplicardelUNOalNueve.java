6c9ecdfad7b2a88d8ce75f7c9219361f1596885f
Joel García Pérez <158473724+JG1323@users.noreply.github.com>
2024-02-13 10:03:38 +0100

    +++

---
+++ Ejercicios extra 8 de febrero/src/TablasdemultiplicardelUNOalNueve.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+public class TablasdemultiplicardelUNOalNueve {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+
+	       
+        System.out.print("Ingresa el número para la tabla de multiplicar: ");
+        int inicio = scanner.nextInt();
+
+        System.out.println("Ingresa el numero final: ");
+        	int fin = scanner.nextInt();
+        
+        int contador = 1;
+
+        
+        System.out.println("Tabla de multiplicar del " + inicio + "al " + fin + ":");
+
+ for (int i= inicio; i <=fin; i++) {
+        System.out.println("Tabla de multiplicar del" + i + ":");
+        System.out.println("----------------------------------");
+        
+        int contador2 =1;
+        
+        while (contador <= 10) {
+            
+            int resultado = i * contador2;
+
+ System.out.println(inicio + " * " + contador2 + " = " + resultado);
+ 		
+ 		contador++;
+       
+        
+        
+        }
+
+       
+        scanner.close();
+    
+
+        
+}
+
+
+	}
+	
+}
+
+
+
---
+++ ZZZ/src/Pruebas.java
@@ -0,0 +1,17 @@
+
+
+public class Pruebas {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int num =5;
+		while (num >=5) {
+			System.out.println(num);
+			num++;
+		}
+		
+
+
+	}
+	}
