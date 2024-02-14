785f8f0b0e6cf26bccaf45f12513c2ef5d3d02fe
Joel García Pérez <158473724+JG1323@users.noreply.github.com>
2024-02-14 09:19:21 +0100

    +++

---
+++ Ejercicios extra 8 de febrero/src/DetectarsiesunPalindr0m0.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class DetectarsiesunPalindr0m0 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	String palin="zorra";
+		
+		Scanner scanner= new Scanner(System.in);
+		
+		System.out.println("Introduzca una cadena para saber si es palindromo:");
+		
+	String input= scanner.nextLine();
+
+		if (esPalindromo(input)) {
+			System.out.println("Es palíndromo" + palin);
+		} else {
+				System.out.println("No es un palíndromo");
+			}
+		scanner.close();
+	}
+	
+	private static boolean esPalindromo(String cadena) {
+		// TODO Auto-generated method stub
+		cadena = cadena.replaceAll("s+","").toLowerCase();
+		return cadena.equals(new StringBuilder(cadena).reverse().toString());
+	}
+		
+	} 
--- ZZZ/src/Pruebas.java
+++ ZZZ/src/Pruebas.java
@@ -1,17 +1,12 @@
 
-
 public class Pruebas {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		int num =5;
-		while (num >=5) {
-			System.out.println(num);
-			num++;
+		
 		}
 		
 
 
-	}
 	}
