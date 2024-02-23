5877d9fd7c7b9bb18a2e2af39ceef381dc2993ba
Joel García Pérez <158473724+JG1323@users.noreply.github.com>
2024-02-23 13:17:08 +0100

    .

--- T6/src/TA6ConversiondeMoneda.java
+++ T6/src/TA6ConversiondeMoneda.java
@@ -1,28 +1,28 @@
 
 public class TA6ConversiondeMoneda {
-    public static void main(String[] args) {
-        double euros = 10; 
-        String monedaDestino = "dolares"; 
-        
-        convertirMoneda(euros, monedaDestino);
-    }
-    
-    public static void convertirMoneda(double euros, String monedaDestino) {
-        double tasaConversion = 0;
-        String nombreMoneda = "";
-        
-        switch (monedaDestino.toLowerCase()) {
-            case "dolares":
-                tasaConversion = 1.13; 
-                nombreMoneda = "dólares";
-                break;
-         
-            default:
-                System.out.println("Moneda incorrecta. Pon dolares, yenes o libras.");
-                return;
-        }
-        
-        double cantidadConvertida = euros * tasaConversion;
-        System.out.println(euros + " euros equivale a " + cantidadConvertida + " " + nombreMoneda + ".");
-    }
+	public static void main(String[] args) {
+		double euros = 10;
+		String monedaDestino = "dolares";
+
+		convertirMoneda(euros, monedaDestino);
+	}
+
+	public static void convertirMoneda(double euros, String monedaDestino) {
+		double tasaConversion = 0;
+		String nombreMoneda = "";
+
+		switch (monedaDestino.toLowerCase()) {
+		case "dolares":
+			tasaConversion = 1.13;
+			nombreMoneda = "dólares";
+			break;
+
+		default:
+			System.out.println("Moneda incorrecta. Pon dolares, yenes o libras.");
+			return;
+		}
+
+		double cantidadConvertida = euros * tasaConversion;
+		System.out.println(euros + " euros equivale a " + cantidadConvertida + " " + nombreMoneda + ".");
+	}
 }
