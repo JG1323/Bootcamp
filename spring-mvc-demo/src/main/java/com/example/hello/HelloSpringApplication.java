a8ca63510aed52b2e020b90153b27571dd261455
Joel García Pérez <perezjoelgarcia13@gmail.com>
2024-06-14 10:49:57 +0200

    tema23

---
+++ spring-mvc-demo/src/main/java/com/example/hello/HelloSpringApplication.java
@@ -0,0 +1,10 @@
+package com.example.hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+public class HelloSpringApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HelloSpringApplication.class, args);
+    }
+}
---
+++ spring-mvc-demo/src/main/java/org/springframework/boot/SpringApplication.java
@@ -0,0 +1,12 @@
+package org.springframework.boot;
+
+import com.example.hello.HelloSpringApplication;
+
+public class SpringApplication {
+
+	public static void run(Class<HelloSpringApplication> class1, String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
